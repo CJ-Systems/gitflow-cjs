1c9b5b4286361ee88d64305e5a0080f03570cc5e
ChrisjStone <chrisjstone79@gmail.com>
2023-07-02 21:59:10 -0600

    test commit for gitguardian

---
+++ bucket_s3.py
@@ -0,0 +1,25 @@
+# Save data to an AWS bucket
+
+from typing import Dict
+
+import aws_lib
+import pymongo
+
+
+def aws_upload(data: Dict):
+    database = aws_lib.connect("AKIAF6BAFJKR45SAWSZ5", "hjshnk5ex5u34565AWS654/JKGjhz545d89sjkja")
+    database.push(data)
+
+
+def transform_data(es_data: Dict) -> Dict:
+    es_data = {**data, "origin": "ES"}
+
+MONGO_URI = "mongodb+srv://testuser:hub24aoeu@gg-is-awesome-gg273.mongodb.net/test?retryWrites=true&w=majority"
+
+def pull_data_from_mongo(query: Dict):
+    return pymongo.connect(MONGO_URI).fetch(query)
+
+
+def push_mongo_to_s3(query):
+    for element in pull_data_from_mongo(query):
+        upload(element)
